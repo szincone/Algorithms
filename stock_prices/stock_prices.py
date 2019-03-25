5f7060e5dc98b0e4c8ad04065b7392cd95e5e46a
szincone <sawyerzincone@gmail.com>
2019-03-25 16:52:58 -0600

    finally got stock_ps, left old code

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,11 +4,55 @@ import argparse
 
 
 def find_max_profit(prices):
-    # grab first item in list
-    # first item minus next item
-    # if profit is greater than previously, replace profit v
-    # else keep iteratting
-    pass
+    # # grab first item in list
+    # first_item = prices[0]
+    # result = 0
+    # # first item minus next item
+    # for x in range(len(prices)):
+    #     temp = prices[x]
+    #     for y in range(len(prices)):
+    #         if temp - prices[y] > result:
+    #             result = temp - prices[y]
+    # return result
+
+    # first_item = prices[0]
+    # profit_bool = True
+    # profit = prices[0] - prices[1]
+    # # first item minus next item
+    # for x in range(1, len(prices) - 1):
+    #     if profit < prices[x] - prices[x + 1]:
+    #         profit = prices[x] - prices[x + 1]
+    #     # if profit_bool is True:
+    #     #     profit += x
+    #     print("PROFIT", profit)
+    # return profit
+    profit_list, profit, loss = [], [], []
+    down_bool = False
+    for i in range(len(prices) - 1):
+        if prices[i] > prices[i + 1]:
+            down_bool = True
+        elif prices[i] < prices[i + 1]:
+            down_bool = False
+        if down_bool is True:
+            profit.append(prices[i + 1] - prices[i])
+        elif down_bool is False:
+            loss.append(prices[i + 1] - prices[i])
+        if down_bool is True and len(loss) > 0:
+            profit_list.append(sum(x for x in loss))
+            loss = []
+        elif down_bool is False and len(profit) > 0:
+            profit_list.append(sum(x for x in profit))
+            profit = []
+        if i == len(prices) - 2 and len(profit_list) == 0:
+            profit_list.append(profit)
+            profit_list.append(loss)
+    if isinstance(max(profit_list), int):
+        return max(profit_list)
+    else:
+        if len(profit_list[0]) > 0:
+            return max([x for x in profit_list[0]])
+        else:
+            return max([x for x in profit_list[1]])
 
 
 if __name__ == '__main__':
