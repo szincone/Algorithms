2ea66e43734e310dd7172566428db79b1cc79770
szincone <sawyerzincone@gmail.com>
2019-03-26 11:37:13 -0600

    working rps, fails tests due to max recursion depth

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,27 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    rps_list = ['rock', 'paper', 'scissors']
+    result = []
+
+    def rounds(rps_order, round_number):
+        r = rps_order
+        for i in range(len(rps_list)):
+            r.append(rps_list[i])
+            if (round_number == n):
+                result.append(r[:])
+            else:
+                rounds(r, round_number + 1)
+            r.pop()
+    rounds([], 1)
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
